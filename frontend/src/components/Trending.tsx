8a51bec8b38d72918afeb32d6ba51a70d6ba0fc6
krishna Sarma <krishsarma03@gmail.com>
2024-03-17 23:49:04 +0530

    warning of trending component fix

--- frontend/src/components/Trending.tsx
+++ frontend/src/components/Trending.tsx
@@ -1,5 +1,3 @@
-import axios from "axios"
-import { useEffect, useState } from "react";
 import { TrendingPostCard } from "./TendingPostCard";
 import { IoIosTrendingUp } from "react-icons/io";
 import { useGetBlogs } from "../hooks";
